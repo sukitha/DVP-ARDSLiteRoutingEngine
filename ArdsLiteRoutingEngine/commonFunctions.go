cb8851b45b0670a7ca2e9a7bd0901c7321f1c1b8
heshanindika <k.heshan.indika@gmail.com>
2015-10-15 10:30:11 +0530

    change host creation

--- ArdsLiteRoutingEngine/commonFunctions.go
+++ ArdsLiteRoutingEngine/commonFunctions.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func AppendIfMissingReq(dataList []Request, i Request) []Request {
 	for _, ele := range dataList {
 		if ele == i {
@@ -16,4 +21,13 @@ func AppendIfMissingString(dataList []string, i string) []string {
 		}
 	}
 	return append(dataList, i)
+}
+
+func CreateHost(_ip, _port string) string {
+	testIp := net.ParseIP(_ip)
+	if testIp.To4() == nil {
+		return _ip
+	} else {
+		return fmt.Sprintf("%s:%s", _ip, _port)
+	}
 }
