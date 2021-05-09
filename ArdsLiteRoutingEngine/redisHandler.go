7074a03a4c31070e7091a162221cfc9c2ed732f6
duoacoder <duoacoder@gmail.com>
2021-05-09 07:07:12 +0530

    replace radix with go-redis

--- ArdsLiteRoutingEngine/redisHandler.go
+++ ArdsLiteRoutingEngine/redisHandler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -9,14 +10,8 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-	"sync"
 
-	// "github.com/mediocregopher/radix.v2/pool"
-	// "github.com/mediocregopher/radix.v2/redis"
-	// "github.com/mediocregopher/radix.v2/sentinel"
-	// "github.com/mediocregopher/radix.v2/util"
-
-	"github.com/mediocregopher/radix/v3"
+	"github.com/go-redis/redis/v8"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -43,11 +38,15 @@ var ardsServicePort string
 var useAmqpAdapter string
 var useDynamicPort string
 
-var redisSentinel *radix.Sentinel
-var redisPool *radix.Pool;
-var redisScanClient radix.Client;
-var redisScanSentinel *radix.Sentinel
-var mu sync.Mutex
+// var redisSentinel *radix.Sentinel
+// var redisPool *radix.Pool;
+// var redisScanClient radix.Client;
+// var redisScanSentinel *radix.Sentinel
+// var mu sync.Mutex
+
+var connectionOptions redis.UniversalOptions
+var redisCtx = context.Background()
+var rdb redis.UniversalClient
 
 
 func errHndlr(err error) {
@@ -237,23 +236,6 @@ func InitiateRedis() {
 	log.Println("useMsgQueue:", useMsgQueue)
 	log.Println("useAmqpAdapter:", useAmqpAdapter)
 
-	var err error
-	customConnFunc := func(network, addr string) (radix.Client, error) {
-		return radix.Dial(network, addr,
-		radix.DialAuthPass(redisPassword),
-	 	radix.DialSelectDB(redisDb),)
-		
-	}
-
-	customPoolConnFunc := func(network, addr string) (radix.Conn, error) {
-		return radix.Dial(network, addr,
-		radix.DialAuthPass(redisPassword),
-	 	radix.DialSelectDB(redisDb),)
-		
-	}
-	// ,
-	// 		radix.DialAuthPass(redisPassword),
-	// 		radix.DialSelectDB(redisDb),
 
 	if redisMode == "sentinel" {
 
@@ -263,7 +245,6 @@ func InitiateRedis() {
 		if len(sentinelIps) > 1 {
 
 			for _, ip := range sentinelIps{
-				//redis://user:secret@localhost:6379/0
 				ipPortArray := strings.Split(ip, ":")
 				sentinelIp := ip;
 				if(len(ipPortArray) > 1){
@@ -275,154 +256,111 @@ func InitiateRedis() {
 				
 			}
 
-			redisSentinel, err = radix.NewSentinel(redisClusterName, ips , radix.SentinelPoolFunc(customConnFunc))
-			redisScanSentinel, err = radix.NewSentinel(redisClusterName, ips , radix.SentinelPoolFunc(customConnFunc))
+			connectionOptions.Addrs = ips
+			connectionOptions.MasterName = redisClusterName
 
-			if err != nil {
-				log.Println("InitiateSentinel ::", err)
-				os.Exit(0)
-			}
 		} else {
-			log.Println("Not enough sentinel servers")
+			fmt.Println("Not enough sentinel servers")
 			os.Exit(0)
 		}
 
 
 	} else {
 
-		redisPool, err = radix.NewPool("tcp", redisIp, 10, radix.PoolConnFunc(customPoolConnFunc))
+		redisIps := strings.Split(redisIp, ",")
+		var ips []string;
+		if len(redisIps) > 0 {
 
-		var errc error
-		redisScanClient, errc = customConnFunc("tcp",redisIp);
-
-		if err != nil {
-			errHndlrNew("InitiateRedis", "InitiatePool", err)
-			os.Exit(0)
-		}
-
-
-		if errc != nil {
-			errHndlrNew("InitiateRedis", "InitiateScanner", err)
-			os.Exit(0)
-		}
-	}
-}
-
-// Redis String Methods
-
-func Cmd(cmd radix.CmdAction) error{
-
-	var err error;
-	if redisMode == "sentinel" {
+			for _, ip := range redisIps{
+				ipPortArray := strings.Split(ip, ":")
+				redisAddr := ip;
+				if(len(ipPortArray) > 1){
+					redisAddr = fmt.Sprintf("%s:%s", ipPortArray[0], ipPortArray[1])
+				}else{
+					redisAddr = fmt.Sprintf("%s:%s", ip, redisPort)
+				}
+				ips = append(ips, redisAddr)
 				
-		if err := redisSentinel.Do(cmd); err != nil {
-			fmt.Println(err)
-		}
+			}
 
-	} else {
-			
-		if err := redisPool.Do(cmd); err != nil {
-			errHndlrNew("OnReset", "getConnFromPool", err)
+			connectionOptions.Addrs = ips
+
+		} else {
+			fmt.Println("Not enough redis servers")
+			os.Exit(0)
 		}
 	}
 
-	return err;
+	connectionOptions.DB = redisDb
+	connectionOptions.Password = redisPassword
 
+	rdb = redis.NewUniversalClient(&connectionOptions)
+
+	
 }
+
 
 func RedisSet(key, value string) string {
 
-	
-	var setVar string;
-	cmd := radix.Cmd(&setVar, "SET", key, value)
-
-	Cmd(cmd);
-    
-	return setVar
+	result, _:= rdb.Set(context.TODO(), key, value, 0).Result()
+	return result
 
 }
 
 func RedisGet(key string) string {
-	var setVar string;
-	cmd := radix.Cmd(&setVar, "GET", key)
 
-	Cmd(cmd);
-    
-	return setVar
+	//cmd := radix.Cmd(&setVar, "GET", key)
+
+	result, _:= rdb.Get(context.TODO(),key).Result()
+	return result
 
 }
 
 func RedisGet_v1(key string) (strObj string, err error) {
 	
-	var setVar string;
-	cmd := radix.Cmd(&setVar, "GET", key)
-
-	err = Cmd(cmd);
-	strObj = setVar;
-    
+	//cmd := radix.Cmd(&setVar, "GET", key)
+	strObj, err = rdb.Get(context.TODO(), key).Result()
 	return 
 
 }
 
 func RedisSearchKeys(pattern string) []string {
 
-	mu.Lock();
 
 	defer func() {
-		mu.Unlock()
+
 		if r := recover(); r != nil {
 			fmt.Println("Recovered in ScanAndGetKeys", r)
 		}
 	}()
 
-	
-	scanOpts := radix.ScanOpts{
-		Command: "SCAN",
-		Count: 1000,
-		Pattern: pattern,
-	}
+
 
 	matchingKeys := make([]string, 0)
 
 	log.Println("Start ScanAndGetKeys:: ", pattern)
 
-	var client  radix.Client;
+	var ctx = context.TODO()
+	iter := rdb.Scan(ctx, 0, pattern, 1000).Iterator()
+	for iter.Next(ctx) {
+		
+		AppendIfMissing(matchingKeys, iter.Val())
+	}
+	if err := iter.Err(); err != nil {
 
-	if redisMode == "sentinel" {
-		addr, _ := redisScanSentinel.Addrs()
-		client , _ = redisScanSentinel.Client(addr) 
-	}else{
-		client = redisScanClient;
+		fmt.Println("ScanAndGetKeys","SCAN",err)
 	}
 
-	if client != nil{
-		scanner := radix.NewScanner(client , scanOpts)
-		var key string;
-		counter := 0;
-
-
-		for scanner.Next(&key) {
-			counter++
-			matchingKeys = AppendIfMissing(matchingKeys, key)
-		}
-
-		if err := scanner.Close(); err != nil{
-			fmt.Println(err)
-			os.Exit(0)
-		}
-    }
 
 	return matchingKeys
 }
 
 func RedisSetNx(key, value string, timeout int) bool {
 
-	var setVar string;
-	cmd := radix.Cmd(&setVar, "SET", key, value, "nx", "ex", strconv.Itoa(timeout))
-
-	 Cmd(cmd);
+	//cmd := radix.Cmd(&setVar, "SET", key, value, "nx", "ex", strconv.Itoa(timeout))
+	 result, _ := rdb.Do(context.TODO(),"SET", key, value, "nx", "ex", timeout).Text()
 	
-    if setVar == "OK" {
+    if result == "OK" {
 		log.Println("GetRLock: ", true)
 		return true
 	} else {
@@ -432,16 +370,37 @@ func RedisSetNx(key, value string, timeout int) bool {
 }
 
 
+/*
+
+IncrByXX := redis.NewScript(`
+		if redis.call("GET", KEYS[1]) ~= false then
+			return redis.call("INCRBY", KEYS[1], ARGV[1])
+		end
+		return false
+	`)
+
+n, err := IncrByXX.Run(ctx, rdb, []string{"xx_counter"}, 2).Result()
+fmt.Println(n, err)
+
+err = rdb.Set(ctx, "xx_counter", "40", 0).Err()
+if err != nil {
+	panic(err)
+}
+
+n, err = IncrByXX.Run(ctx, rdb, []string{"xx_counter"}, 2).Result()
+
+*/
+
 
 func RedisRemoveRLock(key, value string) bool {
 
-    var setVar int;
-	luaScript := "if redis.call('get',KEYS[1]) == ARGV[1] then return redis.call('del',KEYS[1]) else return 0 end"
-	cmd := radix.Cmd(&setVar, "EVAL", luaScript, strconv.Itoa(1), key, value)
 
-	 Cmd(cmd);
+	luaScript := "if redis.call('get',KEYS[1]) == ARGV[1] then return redis.call('del',KEYS[1]) else return 0 end"
+	//cmd := radix.Cmd(&setVar, "EVAL", luaScript, strconv.Itoa(1), key, value)
+	result, _ := rdb.Do(context.TODO(),"EVAL", luaScript, strconv.Itoa(1), key, value).Int()
+
 	
-    if setVar == 1 {
+    if result == 1 {
 		log.Println("GetRLock: ", true)
 		return true
 	} else {
@@ -455,13 +414,12 @@ func RedisRemoveRLock(key, value string) bool {
 
 func RedisRemove(key string) bool {
 	
-	
-	var setVar string;
-	cmd := radix.Cmd(&setVar, "DEL", key)
+	//cmd := radix.Cmd(&setVar, "DEL", key)
 
-	 Cmd(cmd);
+	result, _ := rdb.Del(context.TODO(), key).Result()
+
 	
-    if result, _ := strconv.Atoi(setVar); result == 1 {
+    if result == 1 {
 		log.Println("Remove Key: ", true)
 		return true
 	} else {
@@ -472,12 +430,13 @@ func RedisRemove(key string) bool {
 }
 
 func RedisCheckKeyExist(key string) bool {
-	var setVar int;
-	cmd := radix.Cmd(&setVar, "EXISTS", key)
 
-	 Cmd(cmd);
+	//cmd := radix.Cmd(&setVar, "EXISTS", key)
+
+	result, _ := rdb.Exists(context.TODO(), key).Result()
+
 	
-    if setVar == 1 {
+    if result == 1 {
 		log.Println("Remove Key: ", true)
 		return true
 	} else {
@@ -490,26 +449,26 @@ func RedisCheckKeyExist(key string) bool {
 // Redis Hashes Methods
 
 func RedisHashGetAll(hkey string) map[string]string {
-	var setVar map[string]string;
-	cmd := radix.Cmd(&setVar, "HGETALL", hkey)
-	Cmd(cmd);
-	return setVar
+	//var setVar map[string]string;
+	//cmd := radix.Cmd(&setVar, "HGETALL", hkey)
+	result, _ := rdb.HGetAll(context.TODO(), hkey).Result()
+	return result
 }
 
 func RedisHashGetValue(hkey, queueId string) string {
-	var setVar string;
-	cmd := radix.Cmd(&setVar, "HGET", hkey, queueId)
-	 Cmd(cmd);
-	return setVar
+	//var setVar string;
+	//cmd := radix.Cmd(&setVar, "HGET", hkey, queueId)
+	result, _ := rdb.HGet(context.TODO(), hkey, queueId).Result()
+	return result
 }
 
 func RedisHashSetField(hkey, field, value string) bool {
 	
-	var setVar int;
-	cmd := radix.Cmd(&setVar, "HSET", hkey, field, value)
-	 Cmd(cmd);
+	//var setVar int;
+	//cmd := radix.Cmd(&setVar, "HSET", hkey, field, value)
+	result, _ := rdb.HSet(context.TODO(),hkey, field, value).Result()
 
-	if setVar == 1 {
+	if result == 1 {
 		return true
 	} else {
 		return false
@@ -518,10 +477,11 @@ func RedisHashSetField(hkey, field, value string) bool {
 
 func RedisRemoveHashField(hkey, field string) bool {
 	
-	var setVar int;
-	cmd := radix.Cmd(&setVar, "HDEL", hkey, field)
-	Cmd(cmd);
-    if setVar == 1 {
+	
+	//cmd := radix.Cmd(&setVar, "HDEL", hkey, field)
+	result, _ := rdb.HDel(context.TODO(),hkey, field).Result()
+	
+    if result == 1 {
 		log.Println("Remove Key: ", true)
 		return true
 	} else {
@@ -534,10 +494,11 @@ func RedisRemoveHashField(hkey, field string) bool {
 // Redis List Methods
 
 func RedisListLpop(lname string) string {
-	var setVar string;
-	cmd := radix.Cmd(&setVar, "LPOP", lname)
-	Cmd(cmd);
-	return setVar
+	
+	
+	//cmd := radix.Cmd(&setVar, "LPOP", lname)
+	result, _ := rdb.LPop(context.TODO(),lname).Result()
+	return result
 }
 
 
@@ -547,8 +508,8 @@ func RedisGeoRadius(tenant, company int, locationObj ReqLocationData) [][]string
 	var setVar [][]string;
 	locationInfoKey := fmt.Sprintf("location:%d:%d", tenant, company)
 	log.Println("locationInfoKey: ", locationInfoKey)
-	cmd := radix.Cmd(&setVar, "georadius", "positions", fmt.Sprintf("%.6f",locationObj.Longitude) , fmt.Sprintf("%.6f",locationObj.Latitude), strconv.Itoa(locationObj.Radius)  , locationObj.Metric, "WITHDIST", "ASC")
-	Cmd(cmd);
+	//cmd := radix.Cmd(&setVar, "georadius", "positions", fmt.Sprintf("%.6f",locationObj.Longitude) , fmt.Sprintf("%.6f",locationObj.Latitude), strconv.Itoa(locationObj.Radius)  , locationObj.Metric, "WITHDIST", "ASC")
+	
 	return setVar
 
 }
@@ -556,9 +517,11 @@ func RedisGeoRadius(tenant, company int, locationObj ReqLocationData) [][]string
 func RoutingEngineDistribution(pubChannelName string) string {
 
 
-	var activeRoutingKey string;
-	cmd := radix.Cmd(&activeRoutingKey, "GET", "ActiveRoutingEngine")
-	Cmd(cmd);
+	//var activeRoutingKey string;
+	//cmd := radix.Cmd(&activeRoutingKey, "GET", "ActiveRoutingEngine")
+	//Cmd(cmd);
+
+	activeRoutingKey, _ := rdb.Get(context.TODO(), "ActiveRoutingEngine").Result()
 
 	if activeRoutingKey == "" {
 		u1 := uuid.NewV4()
@@ -577,11 +540,11 @@ func RoutingEngineDistribution(pubChannelName string) string {
 	} else {
 
 		if activeRoutingKey == pubChannelName {
-			var expire int;
-	        cmd := radix.Cmd(&expire, "EXPIRE", "ActiveRoutingEngine", "60")
-	        Cmd(cmd);
+			
+	        //cmd := radix.Cmd(&expire, "EXPIRE", "ActiveRoutingEngine", "60")
+	        expire, _ := rdb.Expire(context.TODO(), "ActiveRoutingEngine", 60).Result()
 
-			if expire == 1 {
+			if expire {
 				//log.Println("Extend Active Routing Engine Expire Time Success")
 			} else {
 				log.Println("Extend Active Routing Engine Expire Time Failed")
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/DuoSoftware/gorest v0.0.0-20150117130044-c510fe9f60cb
+	github.com/go-redis/redis/v8 v8.8.2 // indirect
 	github.com/mediocregopher/radix.v2 v0.0.0-20181115013041-b67df6e626f9
 	github.com/mediocregopher/radix/v3 v3.7.0 // indirect
 	github.com/satori/go.uuid v1.2.0
